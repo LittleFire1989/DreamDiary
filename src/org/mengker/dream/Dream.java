830c36cdc8ba22bc9ecc3d7c51110a72316d4f8c
LittleFire1989 <dancedfire@163.com>
2013-11-10 16:36:04 -0700

    New class dream and recording.

---
+++ src/org/mengker/dream/Dream.java
@@ -0,0 +1,21 @@
+package org.mengker.dream;
+
+import java.util.Date;
+
+public class Dream {
+	private String[] keywords;
+	private short lucidity;
+	private Date sleepStartTime;
+	private Date sleepEndTime;
+	private Date lastEditedTime;
+	private String note;
+	private Recording[] recordings;
+	
+	public void publish(){
+		
+	}
+	
+	public void editDream(){
+		
+	}
+}
