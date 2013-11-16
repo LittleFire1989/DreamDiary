b0748c293b7071e2bdf4007ddb884e824239c033
zhanganqi <zhanganqi@dhcp-10-132-134-23.uawifi.arizona.edu>
2013-11-16 13:39:21 -0700

    modify_mandy

--- gen/org/mengker/dreamdiary/R.java
+++ gen/org/mengker/dreamdiary/R.java
@@ -16,51 +16,75 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040006;
-        public static final int activity_vertical_margin=0x7f040007;
+        public static final int activity_horizontal_margin=0x7f040005;
+        public static final int activity_vertical_margin=0x7f040006;
+        public static final int bottom_tab_font_size=0x7f040009;
+        public static final int bottom_tab_padding_drawable=0x7f040007;
+        public static final int bottom_tab_padding_up=0x7f040008;
         /**  Default screen margins, per the Android Design guidelines. 
          */
-        public static final int login_horizontal_margin=0x7f040000;
-        public static final int login_vertical_margin=0x7f040001;
-        public static final int record_horizontal_margin=0x7f040004;
-        public static final int record_vertical_margin=0x7f040005;
-        public static final int register_horizontal_margin=0x7f040002;
-        public static final int register_vertical_margin=0x7f040003;
+        public static final int login_vertical_margin=0x7f040000;
+        public static final int record_horizontal_margin=0x7f040003;
+        public static final int record_vertical_margin=0x7f040004;
+        public static final int register_horizontal_margin=0x7f040001;
+        public static final int register_vertical_margin=0x7f040002;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int amsu_menu_bg_d=0x7f020000;
+        public static final int amsu_menu_bg_n=0x7f020001;
+        public static final int amsu_menu_bg_s=0x7f020002;
+        public static final int amsu_menu_diary=0x7f020003;
+        public static final int amsu_menu_email=0x7f020004;
+        public static final int amsu_menu_home=0x7f020005;
+        public static final int amsu_menu_publish=0x7f020006;
+        public static final int amsu_menu_record=0x7f020007;
+        public static final int amsu_menu_search=0x7f020008;
+        public static final int amsu_menu_toolbar_bg=0x7f020009;
+        public static final int amsu_menu_user=0x7f02000a;
+        public static final int default_bg=0x7f02000b;
+        public static final int hm_button=0x7f02000c;
+        public static final int ic_launcher=0x7f02000d;
+        public static final int login_background=0x7f02000e;
     }
     public static final class id {
-        public static final int action_settings=0x7f080017;
-        public static final int addEmailEditText=0x7f080013;
-        public static final int addPasswordEditText=0x7f080014;
-        public static final int addUsernameEditText=0x7f080012;
-        public static final int createAccountTextView=0x7f080011;
-        public static final int createButton=0x7f080016;
-        public static final int doneButton=0x7f08000b;
+        public static final int  @+id/linearLayout=0x7f080003;
+        public static final int action_settings=0x7f08001d;
+        public static final int addEmailEditText=0x7f080019;
+        public static final int addPasswordEditText=0x7f08001a;
+        public static final int addUsernameEditText=0x7f080018;
+        public static final int createAccountTextView=0x7f080017;
+        public static final int createButton=0x7f08001c;
+        public static final int doneButton=0x7f080011;
         public static final int facebookLoginButton=0x7f080009;
-        public static final int halfLucidRadio=0x7f08000f;
-        public static final int loginButton=0x7f080007;
-        public static final int lucidRadio=0x7f080010;
-        public static final int moreOptionButton=0x7f08000c;
-        public static final int normalRadio=0x7f08000e;
-        public static final int passwordEditText=0x7f080005;
-        public static final int paswordTextView=0x7f080008;
-        public static final int rePassowrdEditText=0x7f080015;
+        public static final int halfLucidRadio=0x7f080015;
+        public static final int loginButton=0x7f080008;
+        public static final int lucidRadio=0x7f080016;
+        public static final int main_tab=0x7f08000c;
+        public static final int moreOptionButton=0x7f080012;
+        public static final int normalRadio=0x7f080014;
+        public static final int passwordEditText=0x7f080007;
+        public static final int paswordTextView=0x7f080006;
+        public static final int radio_button0=0x7f08000d;
+        public static final int radio_button1=0x7f08000e;
+        public static final int radio_button2=0x7f08000f;
+        public static final int radio_button3=0x7f080010;
+        public static final int rePassowrdEditText=0x7f08001b;
         public static final int registerButton=0x7f08000a;
-        public static final int selectRadioGroup=0x7f08000d;
-        public static final int skipButton=0x7f080006;
+        public static final int selectRadioGroup=0x7f080013;
+        public static final int skipButton=0x7f08000b;
         public static final int testLoginActivityButton=0x7f080000;
         public static final int testRecordActivityButton=0x7f080002;
         public static final int testRegisterActivityButton=0x7f080001;
-        public static final int usernameEditText=0x7f080003;
+        public static final int usernameEditText=0x7f080005;
         public static final int usernameTextView=0x7f080004;
     }
     public static final class layout {
         public static final int activity_index=0x7f030000;
         public static final int activity_login=0x7f030001;
-        public static final int activity_record=0x7f030002;
-        public static final int activity_register=0x7f030003;
+        public static final int activity_main=0x7f030002;
+        public static final int activity_record=0x7f030003;
+        public static final int activity_register=0x7f030004;
+        public static final int activity_test=0x7f030005;
     }
     public static final class menu {
         public static final int index=0x7f070000;
@@ -70,45 +94,53 @@ public final class R {
         public static final int register=0x7f070004;
     }
     public static final class string {
-        public static final int Half_lucid_radioButton=0x7f050018;
+        public static final int Half_lucid_radioButton=0x7f05001e;
         public static final int action_compose=0x7f050003;
         public static final int action_search=0x7f050002;
         public static final int action_settings=0x7f050001;
-        public static final int add_email_edit_text=0x7f050011;
-        public static final int add_password_edit_text=0x7f050012;
-        public static final int add_username_edit_text=0x7f050010;
+        public static final int add_email_edit_text=0x7f050017;
+        public static final int add_password_edit_text=0x7f050018;
+        public static final int add_username_edit_text=0x7f050016;
         public static final int app_name=0x7f050000;
         /**  register page 
          */
-        public static final int create_account_text_view=0x7f05000f;
-        public static final int create_button=0x7f050014;
+        public static final int create_account_text_view=0x7f050015;
+        public static final int create_button=0x7f05001a;
         /**  record 
          */
-        public static final int done_button=0x7f050015;
-        public static final int facebook_login_button=0x7f05000c;
-        public static final int hello_world=0x7f05001b;
-        public static final int login_button=0x7f05000b;
-        public static final int lucid_radioButton=0x7f050019;
-        public static final int more_option_button=0x7f050016;
-        public static final int normal_radioButton=0x7f050017;
-        public static final int password_edit_text=0x7f05000a;
-        public static final int password_text_view=0x7f050009;
-        public static final int register_button=0x7f05000d;
-        public static final int repassword_edit_text=0x7f050013;
-        public static final int skip_button=0x7f05000e;
+        public static final int done_button=0x7f05001b;
+        public static final int dream_about_me=0x7f05000a;
+        public static final int dream_diary=0x7f050008;
+        public static final int dream_explore=0x7f050009;
+        /**  main 
+         */
+        public static final int dream_record=0x7f050007;
+        public static final int facebook_login_button=0x7f050012;
+        public static final int hello_world=0x7f050021;
+        public static final int login_button=0x7f050011;
+        public static final int lucid_radioButton=0x7f05001f;
+        public static final int more_option_button=0x7f05001c;
+        public static final int normal_radioButton=0x7f05001d;
+        public static final int password=0x7f05000f;
+        public static final int password_edit_text=0x7f050010;
+        public static final int password_text_view=0x7f05000e;
+        public static final int register_button=0x7f050013;
+        public static final int repassword_edit_text=0x7f050019;
+        public static final int skip_button=0x7f050014;
         /**  test page 
          */
         public static final int test_login_activity_button=0x7f050004;
         public static final int test_record_activity_button=0x7f050005;
         public static final int test_register_activity_button=0x7f050006;
-        public static final int title_activity_index=0x7f05001e;
-        public static final int title_activity_login=0x7f05001a;
-        public static final int title_activity_record=0x7f05001c;
-        public static final int title_activity_register=0x7f05001d;
-        public static final int username_edit_text=0x7f050008;
+        public static final int title_activity_index=0x7f050024;
+        public static final int title_activity_login=0x7f050020;
+        public static final int title_activity_record=0x7f050022;
+        public static final int title_activity_register=0x7f050023;
+        public static final int username=0x7f05000c;
+        public static final int username_edit_text=0x7f05000d;
         /**  login page 
          */
-        public static final int username_text_view=0x7f050007;
+        public static final int username_text_view=0x7f05000b;
     }
     public static final class style {
         /** 
@@ -138,5 +170,6 @@ public final class R {
          */
         public static final int AppTheme=0x7f060001;
         public static final int Button=0x7f060002;
+        public static final int main_tab_bottom=0x7f060003;
     }
 }
