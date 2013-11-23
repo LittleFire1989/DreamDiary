00d49dac46d204a85f3e5ba1eaf787e4a3073643
LittleFire1989 <dancedfire@163.com>
2013-11-22 17:00:09 -0700

    Delete DreamNotebook and modified Dream and DreamAnalyzer

--- src/org/mengker/dream/DreamAnalyzer.java
+++ src/org/mengker/dream/DreamAnalyzer.java
@@ -1,31 +1,134 @@
 package org.mengker.dream;
 
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-/*
- * To be modified:
- * Move the analyze methods from Dream to DreamAnalyzer
- */
 
 public class DreamAnalyzer {
-	private float avgLucidityRate;
-	private Calendar avgSleepStartTime;
-	private Calendar avgSleepEndTime;
-	private String[] mostOftenWords;
-
-	public DreamAnalyzer(float avgLucidityRate, Calendar avgSleepStartTime,
-			Calendar avgSleepEndTime, String[] mostOftenWords) {
-		this.avgLucidityRate = avgLucidityRate;
-		this.avgSleepStartTime = avgSleepStartTime;
-		this.avgSleepEndTime = avgSleepEndTime;
-		this.mostOftenWords = mostOftenWords;
+	private List<Dream> dreams;
+	
+	public void setDreams(List<Dream> dreams){
+		this.dreams = dreams;
 	}
 	
-	public DreamAnalyzer(DreamNotebook notebook){
-		this.avgLucidityRate = notebook.avgLucidityRate();
-		this.avgSleepStartTime = notebook.avgSleepStartTime();
-		this.avgSleepEndTime = notebook.avgSleepEndTime();
-//		this.mostOftenWords = notebook.mostOftenWords();
+	public float avgLucidityRate() {
+		int numOfLucidDream = 0;
+		int numOfHalfLucidDream = 0;
+		float avgLucidityRate = 0f;
+
+		if (dreams == null || dreams.size() == 0)
+			return avgLucidityRate;
+		else {
+			for (int i = 0; i < dreams.size(); i++) {
+				if (dreams.get(i).getLucidity() == Dream.LUCID) {
+					numOfLucidDream++;
+					continue;
+				}
+				if (dreams.get(i).getLucidity() == Dream.HALFLUCID) {
+					numOfHalfLucidDream++;
+				}
+			}
+			avgLucidityRate = ((float) numOfLucidDream + 0.5f * numOfHalfLucidDream)
+					/ dreams.size();
+			return avgLucidityRate;
+		}
+	}
+
+	public Calendar avgSleepStartTime() {
+
+		int size = dreams.size();
+		Date[] timeArray = new Date[size];
+		for (int i = 0; i < size; i++) {
+			timeArray[i] = dreams.get(i).getSleepStartTime();
+		}
+		return calcAvgTime(timeArray);
+	}
+
+	public Calendar avgSleepEndTime() {
+		int size = dreams.size();
+		Date[] timeArray = new Date[size];
+		for (int i = 0; i < size; i++) {
+			timeArray[i] = dreams.get(i).getSleepEndTime();
+		}
+		return calcAvgTime(timeArray);
+	}
+
+	private Calendar calcAvgTime(Date[] timeArray) {
+		int length = timeArray.length;
+		GregorianCalendar[] calendars = new GregorianCalendar[length];
+		int total = 0;
+		int avg = 0;
+		int avgHour = 0;
+		int avgMin = 0;
+		GregorianCalendar avgCalendar = new GregorianCalendar();
+
+		for (int i = 0; i < length; i++) {
+			calendars[i].setTime(timeArray[i]);
+			int hour = calendars[i].get(Calendar.HOUR_OF_DAY);
+			int min = calendars[i].get(Calendar.MINUTE);
+			int time = min + hour * 60;
+			total = total + time;
+		}
+
+		avg = total / length;
+		avgHour = avg / 60;
+		avgMin = avg % 60;
+		avgCalendar.set(Calendar.HOUR_OF_DAY, avgHour);
+		avgCalendar.set(Calendar.MINUTE, avgMin);
+
+		return avgCalendar;
+	}
+
+	public Map.Entry<String, Integer>[] mostOftenWords(String[] contents, int topX) {
+
+		HashMap<String, Integer> wordList = new HashMap<String, Integer>();
+		Matcher m;
+		Pattern p = Pattern.compile("\\w+");
+		Entry<String, Integer>[] topWords;
+
+		for (int i = 0; i < contents.length; i++) {
+			String content = contents[i];
+			m = p.matcher(content);
+			while (m.find()) {
+				String word = m.group();
+				if (wordList.containsKey(word)) {
+					wordList.put(word, wordList.get(word) + 1);
+				} else {
+					wordList.put(word, 1);
+				}
+			}
+		}
+		Map.Entry<String, Integer>[] wordArray = (Map.Entry<String, Integer>[])wordList.entrySet().toArray();
+		Arrays.sort(wordArray, new Comparator<Map.Entry<String, Integer>>(){
+
+			public int compare(Entry<String, Integer> o1,
+					Entry<String, Integer> o2) {
+				if(o1.getValue() > o2.getValue())
+					return 1;
+				else if(o1.getValue() == o2.getValue())
+					return 0;
+				else
+					return -1;
+			}
+		});
+		
+		Arrays.sort(wordArray, Collections.reverseOrder());
+		
+		topWords = new Entry[wordArray.length <topX ? wordList.size():topX];
+		for(int i = 0; i < topWords.length; i++){
+			topWords[i] = wordArray[i];
+		}
+		
+		return topWords;
 	}
 }
