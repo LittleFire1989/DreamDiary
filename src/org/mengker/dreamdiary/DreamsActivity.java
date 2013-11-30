62f73671bd3a0e6b601a6f28214b7b70c600be4e
LittleFire1989 <dancedfire@163.com>
2013-11-30 11:11:10 -0700

    Modify dreamsactivity

--- src/org/mengker/dreamdiary/DreamsActivity.java
+++ src/org/mengker/dreamdiary/DreamsActivity.java
@@ -1,15 +1,30 @@
 package org.mengker.dreamdiary;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class DreamsActivity extends Activity {
+	Button createNewDreamDiary;
+	DreamsActivity da = this;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_dreams);
+		createNewDreamDiary = (Button) this.findViewById(R.id.createNewDreamDiary);
+		createNewDreamDiary.setOnClickListener(new OnClickListener(){
+			public void onClick(View arg0) {
+				Intent intent = new Intent(DreamsActivity.this, RecordActivity.class);
+			    startActivity(intent);
+			    finish();
+			}
+		});
+		
 	}
 
 	@Override
--- src/org/mengker/dreamdiary/RecordActivity.java
+++ src/org/mengker/dreamdiary/RecordActivity.java
@@ -14,22 +14,10 @@ import android.widget.TextView.OnEditorActionListener;
 
 public class RecordActivity extends Activity {
 
-	private Dream dream;
-	private int lucidity = Dream.NORMAL;
-	private String content;
-	private RadioGroup lucidityRadioGroup;
-	private TextView testDreamRecordTextView;
-	private EditText dreamRecordEditText;
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_record);
-		lucidityRadioGroup = (RadioGroup) this.findViewById(R.id.lucidityRadioGroup);
-		dreamRecordEditText = (EditText) this.findViewById(R.id.dreamRecordEditText);
-this.testDreamRecordTextView = (TextView) this.findViewById(R.id.testDreamRecordTextView);
-		lucidityRadioGroup.setOnCheckedChangeListener(new LucidityRadioOnCheckedChangeListener());
-		dreamRecordEditText.setOnEditorActionListener(new ContentOnEditorActionListener());
 	}
 
 	@Override
@@ -37,37 +25,6 @@ this.testDreamRecordTextView = (TextView) this.findViewById(R.id.testDreamRecord
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.record, menu);
 		return true;
-	}
-	
-	class LucidityRadioOnCheckedChangeListener implements OnCheckedChangeListener{
-
-		@Override
-		public void onCheckedChanged(RadioGroup group, int checkedId) {
-			switch (checkedId) {
-			case R.id.normalRadio:
-				lucidity = Dream.NORMAL;
-				break;
-			case R.id.halfLucidRadio:
-				lucidity = Dream.HALFLUCID;
-				break;
-			case R.id.lucidRadio:
-				lucidity = Dream.LUCID;
-				break;
-			default:
-				lucidity = Dream.NORMAL;
-			}
-		}
-
-	}
-	
-	class ContentOnEditorActionListener implements OnEditorActionListener{
-
-		@Override
-		public boolean onEditorAction(TextView arg0, int arg1, KeyEvent arg2) {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
 	}
 
 }
