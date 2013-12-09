67c830eac99b0976e929cec1095259e339b88e9d
LittleFire1989 <dancedfire@163.com>
2013-12-08 21:44:02 -0700

    rename edit dream activity

--- src/org/mengker/dreamdiary/EditDreamActivity.java
+++ src/org/mengker/dreamdiary/EditDreamActivity.java
@@ -4,12 +4,12 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class DreamEdit extends Activity {
+public class EditDreamActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_dream_edit);
+		setContentView(R.layout.activity_edit_dream);
 	}
 
 	@Override
