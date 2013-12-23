2109d69f0e12503a057bf11c917993f1dc814b19
LittleFire1989 <dancedfire@163.com>
2013-12-23 12:39:00 -0700

    change to relative layout

--- src/org/mengker/dreamdiary/ExploreActivity.java
+++ src/org/mengker/dreamdiary/ExploreActivity.java
@@ -28,13 +28,13 @@ import android.widget.Toast;
 
 public class ExploreActivity extends Activity {
 	private List<Share> wbList;
-	private LinearLayout loadingLayout=null;
-	
+	private LinearLayout loadingLayout = null;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_explore);
-		
+
 		loadList();
 	}
 
@@ -44,73 +44,59 @@ public class ExploreActivity extends Activity {
 		getMenuInflater().inflate(R.menu.share, menu);
 		return true;
 	}
-	
+
 	private void loadList() {
-		//loadingLayout=(LinearLayout)findViewById(R.id.loadingLayout);
-		
-		
-			// 显示当前用户名称
-			//TextView showName = (TextView) findViewById(R.id.showName);
-			//showName.setText("TestUserName");
+		// loadingLayout=(LinearLayout)findViewById(R.id.loadingLayout);
+
+		// 显示当前用户名称
+		// TextView showName = (TextView) findViewById(R.id.showName);
+		// showName.setText("TestUserName");
 		new Thread(new Runnable() {
 
 			public void run() {
-				Operation operation=new Operation();
-				String result = operation.getShares();	
+				Operation operation = new Operation();
+				String result = operation.getShares();
 				System.out.println("result: " + result);
-				Message msg=new Message();
-				msg.obj=result;
+				Message msg = new Message();
+				msg.obj = result;
 				handler.sendMessage(msg);
 			}
 		}).start();
-			
-			
-				
-				
-				
-				
-				
-		 //loadingLayout.setVisibility(View.GONE);
+
+		// loadingLayout.setVisibility(View.GONE);
 	}
-	
-	Handler handler=new Handler(){
+
+	Handler handler = new Handler() {
 		@Override
 		public void handleMessage(Message msg) {
-			String result=(String) msg.obj;
+			String result = (String) msg.obj;
 			JsonUtil jsonUtil = new JsonUtil();
-			 wbList =  jsonUtil.StringFromJson(result);
+			wbList = jsonUtil.StringFromJson(result);
 
-						//Share w = new Share("shareid","userid","message","username");
-						/*
-						w.setId();
-						w.setUserId(userId);
-						w.setUserName(userName);
-						w.setTime(time);
-						w.setText(text);
+			// Share w = new Share("shareid","userid","message","username");
+			/*
+			 * w.setId(); w.setUserId(userId); w.setUserName(userName);
+			 * w.setTime(time); w.setText(text);
+			 * 
+			 * w.setHaveImage(haveImg); w.setUserIcon(userIcon); wbList.add(w);
+			 */
+			// wbList.add(w);
+			// w = new Share("shareid2","userid2","message2","username2");
+			// wbList.add(w);
 
-						w.setHaveImage(haveImg);
-						w.setUserIcon(userIcon);
-						wbList.add(w);
-						*/
-						//wbList.add(w);
-						// w = new Share("shareid2","userid2","message2","username2");
-						 //wbList.add(w);
-		
+			if (wbList != null) {
 
-		if (wbList != null) {
-			
-			ShareAdapter adapater = new ShareAdapter();
-			ListView Msglist = (ListView) findViewById(R.id.Msglist);
-			
-			Msglist.setAdapter(adapater);
-		
-		}
+				ShareAdapter adapater = new ShareAdapter();
+				ListView Msglist = (ListView) findViewById(R.id.Msglist);
+
+				Msglist.setAdapter(adapater);
+
+			}
 			super.handleMessage(msg);
-		}	
+		}
 	};
-	
+
 	public class ShareAdapter extends BaseAdapter {
-		
 
 		@Override
 		public int getCount() {
@@ -132,31 +118,41 @@ public class ExploreActivity extends Activity {
 
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
-			
+
 			if (convertView == null) {
 				convertView = LayoutInflater.from(getApplicationContext())
 						.inflate(R.layout.share, null);
 			}
 
-			
-			//ImageView tempIV = (ImageView) convertView.findViewById(R.id.wbicon);
-			TextView tempTV = (TextView) convertView.findViewById(R.id.dreamDetailstextView);
-			//wh.wbtime = (TextView) convertView.findViewById(R.id.wbtime);
-			TextView tempUser = (TextView) convertView.findViewById(R.id.userNameTextView);
-			TextView tempDate = (TextView) convertView.findViewById(R.id.dateTextView);
-			//wh.wbimage = (ImageView) convertView.findViewById(R.id.wbimage);
-			ImageView tempImage = (ImageView) convertView.findViewById(R.id.userPhotoImageView);
+			// ImageView tempIV = (ImageView)
+			// convertView.findViewById(R.id.wbicon);
+			TextView tempTV = (TextView) convertView
+					.findViewById(R.id.dreamDetailstextView);
+			// wh.wbtime = (TextView) convertView.findViewById(R.id.wbtime);
+			TextView tempUser = (TextView) convertView
+					.findViewById(R.id.userNameTextView);
+			TextView tempDate = (TextView) convertView
+					.findViewById(R.id.dateTextView);
+			// wh.wbimage = (ImageView) convertView.findViewById(R.id.wbimage);
+			ImageView tempImage = (ImageView) convertView
+					.findViewById(R.id.userPhotoImageView);
 			Share wb = wbList.get(position);
 			if (wb != null) {
 				convertView.setTag(wb.getShareID());
 				tempTV.setText(wb.getMessage());
-				tempUser.setText(wb.getUserName(), TextView.BufferType.SPANNABLE);
+				tempUser.setText(wb.getUserName(),
+						TextView.BufferType.SPANNABLE);
 				tempDate.setText(wb.getPosttime());
-				if(wb.getUserName().equals("allen")) tempImage.setImageResource(R.drawable.allen_photo);
-				else if(wb.getUserName().equals("mandy")) tempImage.setImageResource(R.drawable.mandy_photo);
-				else if(wb.getUserName().equals("YueZhao")) tempImage.setImageResource(R.drawable.yue_photo);
-				else if(wb.getUserName().equals("Rubby")) tempImage.setImageResource(R.drawable.yue_photo);
-				else tempImage.setImageResource(R.drawable.save);
+				if (wb.getUserName().equals("allen"))
+					tempImage.setImageResource(R.drawable.allen_photo);
+				else if (wb.getUserName().equals("mandy"))
+					tempImage.setImageResource(R.drawable.mandy_photo);
+				else if (wb.getUserName().equals("YueZhao"))
+					tempImage.setImageResource(R.drawable.yue_photo);
+				else if (wb.getUserName().equals("Rubby"))
+					tempImage.setImageResource(R.drawable.yue_photo);
+				else
+					tempImage.setImageResource(R.drawable.save);
 
 			}
 
@@ -164,23 +160,22 @@ public class ExploreActivity extends Activity {
 		}
 
 	}
-	
-	//navigate
-	
+
+	// navigate
+
 	public void startDiaryBookActivity(View view) {
-	    Intent intent = new Intent(this, DiaryBookActivity.class);
-	    startActivity(intent);
+		Intent intent = new Intent(this, DiaryBookActivity.class);
+		startActivity(intent);
 	}
-	
+
 	public void startExploreActivity(View view) {
-	    Intent intent = new Intent(this, ExploreActivity.class);
-	    startActivity(intent);
+		Intent intent = new Intent(this, ExploreActivity.class);
+		startActivity(intent);
 	}
-	
-//	public void startMeActivity(View view) {
-//	    Intent intent = new Intent(this, MeActivity.class);
-//	    startActivity(intent);
-//	}
+
+	// public void startMeActivity(View view) {
+	// Intent intent = new Intent(this, MeActivity.class);
+	// startActivity(intent);
+	// }
 
 }
-
